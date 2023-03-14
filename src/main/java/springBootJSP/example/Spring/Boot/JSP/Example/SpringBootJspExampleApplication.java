18f29817802c36529e0be7a8bd9d0fd551002259
Kenan <ksonuksun@gmail.com>
2023-03-14 23:00:10 +0300

    First Commit

---
+++ src/main/java/springBootJSP/example/Spring/Boot/JSP/Example/SpringBootJspExampleApplication.java
@@ -0,0 +1,13 @@
+package springBootJSP.example.Spring.Boot.JSP.Example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJspExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJspExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springBootJSP/example/Spring/Boot/JSP/Example/SpringBootJspExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package springBootJSP.example.Spring.Boot.JSP.Example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJspExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
